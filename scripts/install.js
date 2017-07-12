adb4df1e012d38a3872578d484291b9af07aad5b
Mithgol the Webmaster <getgit@mithgol.ru>
2017-07-12 19:57:53 +0300

    fix nwjs/npm-installer#2 (start using HTTPS: it improves security)

--- scripts/install.js
+++ scripts/install.js
@@ -33,7 +33,7 @@ if ( version.slice(-4) === '-sdk' ){
 }
 
 var url = false;
-var urlBase = process.env.npm_config_nwjs_urlbase || process.env.NWJS_URLBASE ||  'http://dl.nwjs.io/v';
+var urlBase = process.env.npm_config_nwjs_urlbase || process.env.NWJS_URLBASE ||  'https://dl.nwjs.io/v';
 var buildTypeSuffix = buildType === 'normal' ? '' : ('-' + buildType);
 
 // Determine download url
