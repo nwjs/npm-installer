58eee9ac34c9908334c358421427845f4c9780e1
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2023-10-16 10:32:44 -0400

    refactor: `findpath` functionc

--- test/index.mjs
+++ test/index.mjs
@@ -2,7 +2,7 @@ import { strictEqual } from 'node:assert';
 import { existsSync } from 'node:fs';
 import test from 'node:test';
 
-import findpath from '../lib/findpath.js';
+import findpath from '../lib/findpath.mjs';
 
 test('has downloaded and extracted', function() {
   strictEqual(existsSync(findpath()), true);
