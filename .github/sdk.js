f88cb5ff87f5ad71f02be8eecf31c24d84c0c78c
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-03-09 15:09:51 +0530

    chore: fs import

--- .github/sdk.js
+++ .github/sdk.js
@@ -1,7 +1,7 @@
-import { writeFileSync } from 'node:fs';
+import fs from 'node:fs';
 
 import manifest from '../package.json';
 
 manifest.version = manifest.version + '-sdk';
 
-writeFileSync('./package.json', JSON.stringify(manifest, null, 2));
+fs.writeFileSync('./package.json', JSON.stringify(manifest, null, 2));
