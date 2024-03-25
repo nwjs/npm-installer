742aa6f68a2c2b11b94ab7afe921fb9dc4f285b6
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-03-25 23:43:20 +0530

    chore(test): add selenium test

---
+++ test/selenium.test.js
@@ -0,0 +1,32 @@
+import assert from "node:assert";
+import path from "node:path";
+import process from "node:process";
+
+import { By } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
+import { describe, it } from "vitest";
+
+import util from "../src/util.js";
+
+const { Driver, ServiceBuilder, Options } = chrome;
+
+describe("run", async () => {
+  let driver = undefined;
+
+  it("should run after build", async () => {
+    const options = new Options();
+    const args = [
+      `--nwapp=${path.resolve("test", "app")}`,
+      "--headless=new",
+    ];
+    options.addArguments(args);
+
+    const chromedriverPath = util.findpath("chromedriver");
+
+    const service = new ServiceBuilder(chromedriverPath).build();
+
+    driver = Driver.createSession(options, service);
+    const text = await driver.findElement(By.id("test")).getText();
+    assert.strictEqual(text, "Hello, World! Is anyone out there?");
+  }, { timeout: Infinity });
+});
