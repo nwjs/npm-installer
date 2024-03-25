56fdf4e7b26aa7e4539ed92aad05c08bf9a6ec85
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-03-26 00:40:07 +0530

    chore(test): remove chromedriver ttest

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,3 @@ import util from '../src/util.js';
 test('nwjs has downloaded and been extracted', function() {
   expect(fs.existsSync(util.findpath())).toBe(true);
 });
-
-test('chromedriver does not exist in normal build', function() {
-  expect(fs.existsSync(util.findpath('chromedriver'))).toBe(false);
-});
