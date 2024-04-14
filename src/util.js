9b5050ca3201cdb5f08d9178157ea143c42ea472
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-14 12:58:20 +0530

    fix(findpath): windows symlink is not created if not run as Administrator

--- src/util.js
+++ src/util.js
@@ -3,6 +3,8 @@ import path from 'node:path';
 import process from 'node:process';
 import url from 'node:url';
 
+import parse from './parse.js';
+
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 /**
@@ -45,8 +47,10 @@ const EXE_NAME = {
  * @param {'nwjs' | 'chromedriver'} executable Path to NW.js or Chromedriver executable.
  * @return {string}
  */
-function findpath(executable = 'nwjs') {
-    const nwDir = path.resolve(__dirname, '..', 'nwjs');
+function findpath(executable = 'nwjs', options) {
+    options = parse(options);
+    const nwDir = path.resolve(__dirname, '..', `nwjs${options.flavor === "sdk" ? "-sdk" : ""}-v${options.version}-${options.platform}-${options.arch}`,);
+    
     /**
      * File path to executable.
      * 
@@ -55,17 +59,10 @@ function findpath(executable = 'nwjs') {
     let binPath = '';
 
     /**
-      * Host operating system
-      * 
-      * @type {NodeJS.Platform}
-      */
-    let hostOs = PLATFORM_KV[process.env.npm_config_nwjs_platform || process.env.NWJS_PLATFORM || process.platform];
-
-    /**
      * Get the platform dependant path of the NW.js binary.
      */
     function findNwjs() {
-        binPath = path.resolve(nwDir, EXE_NAME[hostOs]);
+        binPath = path.resolve(nwDir, options.platform);
     }
 
     /**
