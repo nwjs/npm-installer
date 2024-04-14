ed0da1cedb14cc0f081136b0120573c2fe1bc290
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-14 13:50:56 +0530

    chore(test): promisify

--- test/findpath.test.js
+++ test/findpath.test.js
@@ -3,7 +3,8 @@ import { expect, test } from 'vitest';
 
 import util from '../src/util.js';
 
-test('nwjs has downloaded and been extracted', async function() {
-  const path = await util.findpath();
-  expect(fs.existsSync(path)).toEqual(true);
+test('nwjs has downloaded and been extracted', function () {
+  util.findpath().then(function (path) {
+    expect(fs.existsSync(path)).toEqual(true);
+  });
 });
