691c3b86f98e5ec9a3966141d6ee9b5e70755b76
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-14 14:31:48 +0530

    fix(test): catch errors

--- test/findpath.test.js
+++ test/findpath.test.js
@@ -6,5 +6,8 @@ import util from '../src/util.js';
 test('nwjs has downloaded and been extracted', function () {
   util.findpath('nwjs').then(function (path) {
     expect(fs.existsSync(path)).toEqual(true);
+  })
+  .catch((error) => {
+    console.log(error);
   });
 });
