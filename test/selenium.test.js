7c5b0f2594fa8d493ce520b35265abcdddd088e4
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-14 14:04:56 +0530

    fix(test): exit selenium test after

--- test/selenium.test.js
+++ test/selenium.test.js
@@ -1,31 +1,41 @@
-import assert from "node:assert";
 import path from "node:path";
 
-import { By } from "selenium-webdriver";
+import selenium from "selenium-webdriver";
 import chrome from "selenium-webdriver/chrome.js";
-import { describe, it } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
 import util from "../src/util.js";
 
 const { Driver, ServiceBuilder, Options } = chrome;
 
-describe("run", async () => {
+describe("run", async function () {
   let driver = undefined;
 
-  it("should run post install", async () => {
-    const options = new Options();
-    const args = [
+  beforeAll(function () {
+    const options = new chrome.Options();
+    const seleniumArgs = [
       `--nwapp=${path.resolve("test", "app")}`,
       "--headless=new",
     ];
-    options.addArguments(args);
+
+    options.addArguments(seleniumArgs);
 
     const chromedriverPath = util.findpath("chromedriver");
-
     const service = new ServiceBuilder(chromedriverPath).build();
 
     driver = Driver.createSession(options, service);
-    const text = await driver.findElement(By.id("test")).getText();
-    assert.strictEqual(text, "Hello, World! Is anyone out there?");
-  }, { timeout: Infinity });
+  });
+
+  it("should run post install", async function () {
+    const textElement = await driver.findElement(selenium.By.id('test'));
+
+    const text = await textElement.getText();
+
+    expect(text).toEqual('Hello, World! Is anyone out there?');
+  });
+
+  afterAll(function () {
+    driver.quit();
+  });
+
 });
