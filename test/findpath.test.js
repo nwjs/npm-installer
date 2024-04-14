0fafa5ccfa1ed958c37a3fa8f247058666f6732e
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-04-14 14:28:34 +0530

    ks;mlds

--- test/findpath.test.js
+++ test/findpath.test.js
@@ -4,7 +4,7 @@ import { expect, test } from 'vitest';
 import util from '../src/util.js';
 
 test('nwjs has downloaded and been extracted', function () {
-  util.findpath().then(function (path) {
+  util.findpath('nwjs').then(function (path) {
     expect(fs.existsSync(path)).toEqual(true);
   });
 });
