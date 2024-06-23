b6eae3a0b272eb4c6e0156283d587fd970786bf4
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2024-06-23 20:17:21 +0530

    chore(test): skip request test

--- src/request.test.js
+++ src/request.test.js
@@ -3,7 +3,7 @@ import { describe, expect, it } from "vitest";
 import request from "./request.js";
 import util from "./util.js";
 
-describe("get/request", function () {
+describe.skip("get/request", function () {
 
   let url = "https://raw.githubusercontent.com/nwutils/nw-builder/main/src/util/osx.arm.versions.json"
   const filePath = "./test/fixture/cache/request.test.json";
