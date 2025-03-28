aa5153803d680fcc92884fc60687037add3998a6
Ayushman Chhabra <14110965+ayushmanchhabra@users.noreply.github.com>
2025-03-29 00:16:43 +0530

    fix(cli): add missing commands

--- src/cli.js
+++ src/cli.js
@@ -17,8 +17,13 @@ async function cli() {
     .option('--flavor <flavor>', 'NW.js flavor')
     .option('--platform <platform>', 'Host platform')
     .option('--arch <arch>', 'Host architecture')
+    .option('--downloadUrl', 'Download server')
     .option('--cacheDir <cacheDir>', 'File path to cache directory')
-    .option('--shaSum', 'Flag to enable/disable shasum checks', true)
+    .option('--cache <boolean>', 'If true the existing cache is used. Otherwise it removes and redownloads it.', false)
+    .option('--ffmpeg <boolean>', 'If true the chromium ffmpeg is replaced by community version with proprietary codecs.', false)
+    .option('--nodeAddon', 'Download Node headers', false)
+    .option('--unref', 'Prevent the parent process from waiting for a given subprocess. This is useful if you are using nw package to call the executable and want to prevent zombie processes eating up memory.', false)
+    .option('--shaSum', 'If true, then shasums are verified. Otherwise, it is ignored.', true)
     .allowExcessArguments(true)
     .allowUnknownOption() // this allows chromium and node options to be passed through to the nwjs binary
     .parse(process.argv);
